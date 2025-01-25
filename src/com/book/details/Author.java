3796ab6a642710d75e6cc8e15459215971231a66
NandaKishore-prog <nanda.kishore@gmail.com>
2025-01-25 17:08:09 +0530

    up

--- src/com/book/details/Author.java
+++ src/com/book/details/Author.java
@@ -5,7 +5,7 @@ public class Author {
 	private int id;
 	private String firstName;
 	private String lastName;
-
+      
 	public Author(int id, String firstName, String lastName) {
 		this.id = id;
 		this.firstName = firstName;
